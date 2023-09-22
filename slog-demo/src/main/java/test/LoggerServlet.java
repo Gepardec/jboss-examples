3ce7ee6636351c259f57d65c76f93bf2d1f44177
Erhard Siegl <erhard.siegl@gepardec.com>
2023-09-22 10:12:32 +0200

    JBoss Deployment

--- slog-demo/src/main/java/test/LoggerServlet.java
+++ slog-demo/src/main/java/test/LoggerServlet.java
@@ -11,8 +11,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.jboss.logging.Logger;
-
 @WebServlet(urlPatterns = "/log")
 public class LoggerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
